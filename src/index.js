76e2592d5ed6641d540259696791b0fc992a98a2
AlexKov90 <kavaleuski.alex@gmail.com>
2018-09-26 20:51:43 +0300

    implement Function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+
+    const elements = equation.split(' ');
+    
+    const a = +elements[0];
+    const b = +(elements[3] + elements[4]);
+    const c = +(elements[7] + elements[8]);
+    const d = Math.pow(b,2) - 4*a*c;
+    
+  
+        
+    const diskr = Math.sqrt(d);
+    const x1 = Math.round(-(b + diskr)/(2*a));
+    const x2 = Math.round(-(b - diskr)/(2*a));
+    return x1 > x2 ? [x2, x1] : [x1, x2];
+    
+    
+    
 }
